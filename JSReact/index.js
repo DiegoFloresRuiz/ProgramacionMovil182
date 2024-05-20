536c45d80e06191a993b5481f7ba8787d4ad30ea
DiegoFloresRuiz <133287417+DiegoFloresRuiz@users.noreply.github.com>
2024-05-20 15:41:41 -0600

    AvancePract2

---
+++ JSReact/index.js
@@ -0,0 +1,34 @@
+console.log('Hola')
+ // funcion 
+/*function suma(a,b){
+    return a+b
+}*/
+
+// Fast Arrow
+/*const suma = (a,b) =>{
+    return a+b
+}*/
+
+// Fast Arrow Simple
+// const suma = (a,b) => a+b
+
+
+//console.log(suma(2,3))
+
+//Objetos 
+
+const usuario={
+    nombre: 'Diego',
+    apellidoP: 'Flores',
+    edad: 20,
+    direccion: { //Objeto dentro del objeto
+        pais:'Mexico',
+        Ciudad: 'Queretaro',
+        calle: 'Conocida'
+    },
+    amigos: {
+        amigos:['Maro', 'Pupe'],
+        estatus: true,
+        envarCorreo: enviarCorr=( ) => 'enviar...'
+    }
+}
