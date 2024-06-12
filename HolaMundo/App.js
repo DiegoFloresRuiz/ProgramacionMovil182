7e48f035ad5c6c03b6bc370bc945c6bf47251c3f
DiegoFloresRuiz <133287417+DiegoFloresRuiz@users.noreply.github.com>
2024-06-12 15:18:38 -0600

    Componenetes
    lo que esta fuera de lo comtado ejecuta el camboo de texto desde el boton

--- HolaMundo/App.js
+++ HolaMundo/App.js
@@ -1,11 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
+/*import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import React,{useState} from 'react';
 
-export default function App() {
+
+
+//con estado
+const Texto = () => {
+  const [contenido, setContenido] = useState('Hola Mundo Soy con estado')
+  const actualizarContenido=()=>{setContenido('set actualizo este texto')}
+  return(<Text onPress={actualizarContenido}>{contenido}</Text>)
+} 
+/*
+con props simple 
+const Texto = (props) => {
+  const {contenido} = props
+  return(<Text>{contenido}</Text>)
+}*/ 
+
+  /*
+  Con children
+  const Texto = (props) => {
+    const {children} = props
+    return(<Text>{children}</Text>)
+  } */
+
+/*export default function App() {
   return (
     <View style={styles.container}>
-      <Text>¡Toy pendeja!</Text>
-      <Button title='Click' color="#000000"></Button>
+      <Texto contenido={'Hola Mundo'}></Texto>
+
+      <Button title='Hoda' color="#000000"></Button>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});*/
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState } from 'react';
+
+const Texto = ({ contenido, actualizarContenido }) => {
+  return <Text onPress={actualizarContenido}>{contenido}</Text>;
+};
+
+export default function App() {
+  const [contenido, setContenido] = useState('Hola Mundo Soy con estado');
+  
+  const actualizarContenido = () => {
+    setContenido('set actualizo este texto');
+  };
+
+  return (
+    <View style={styles.container}>
+      <Texto contenido={contenido} actualizarContenido={actualizarContenido} />
+      <Button title="Actualizar Texto" onPress={actualizarContenido} />
       <StatusBar style="auto" />
     </View>
   );
@@ -19,3 +75,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
