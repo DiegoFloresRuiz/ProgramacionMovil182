ff1fbd7cd71c4d8a8907ddb6d5d8b0c63327201d
DiegoFloresRuiz <133287417+DiegoFloresRuiz@users.noreply.github.com>
2024-06-14 16:26:05 -0600

    finestilos

--- HolaMundo/App.js
+++ HolaMundo/App.js
@@ -47,12 +47,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import React, { useState } from 'react';
 
-const Texto = ({ contenido, actualizarContenido }) => {
-  return <Text onPress={actualizarContenido}>{contenido}</Text>;
+const Texto = ({ contenido, actualizarContenido, estilo }) => {
+  return <Text style={[styles.text,estilo]} onPress={actualizarContenido}>{contenido}</Text>;
 };
 
 export default function App() {
-  const [contenido, setContenido] = useState('Hola Mundo Soy con estado');
+  const [contenido, setContenido] = useState('Hodaaa como tas!');
   
   const actualizarContenido = () => {
     setContenido('set actualizo este texto');
@@ -60,8 +60,10 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Texto contenido={contenido} actualizarContenido={actualizarContenido} />
-      <Button title="Actualizar Texto" onPress={actualizarContenido} />
+      <Texto estilo={styles.black} contenido={contenido} actualizarContenido={actualizarContenido} />
+      <Texto estilo={styles.red} contenido={contenido} actualizarContenido={actualizarContenido} />
+      <Texto estilo={styles.amarillo} contenido={contenido} actualizarContenido={actualizarContenido} />
+      {/* <Button title="Actualizar Texto" onPress={actualizarContenido} /> */}
       <StatusBar style="auto" />
     </View>
   );
@@ -70,9 +72,39 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column', // con row vemos de izquiera a derecha, con column-reverse los vemos al reves y pasa lo mimo con los column
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'center', // flex-start pociona los cuadros a mano izquierda y con end al lado derecho  y el strechtoma todo lo largo dela pantalla
+    justifyContent: 'space-between', // coloca todo ya sea arriba o abajo flex-start y flex-end , space-between (crea espacios entre componentes )
+    
+  },
+
+  text:{
+  color:'white',
+  fontSite:80,
+  },
+  //Creamos tres backgroundColor cada uno de un color distinto para apalicar estos estilos de forma independiente 
+  black:{
+    backgroundColor:'black',
+    width:200,
+    height:50,
+    textAlign:'center',
+    //flex:1,
+  },
+  red:{
+    backgroundColor:'red',
+    width:200,
+    height:50,
+    textAlign:'center',
+    //flex:1,
+  },
+  amarillo:{
+    backgroundColor:'yellow',
+    color:'black',
+    width:200,
+    height:50,
+    textAlign:'center',
+    //flex:1,
   },
 });
 
