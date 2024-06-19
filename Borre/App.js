82d09bfd60d06e059751fd75a682eea1aca7f59a
DiegoFloresRuiz <133287417+DiegoFloresRuiz@users.noreply.github.com>
2024-06-19 15:51:25 -0600

    repaso

--- Borre/App.js
+++ Borre/App.js
@@ -1,20 +1,88 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Alert, Button, alert  } from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [nombre, setName] = useState('');
+  const [ema, setEmail] = useState('');
+  const [pas, setPassword] = useState('');
+
+  const handleSave = () => {
+    Alert.alert(
+      'Formulario Enviado',
+      `Nombre: ${nombre}\nEmail: ${ema}\nPassword: ${pas}`,
+      [
+        {
+          text: 'OK',
+          style: 'default',
+        },
+      ],
+      { cancelable: false }
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.contb}>
+        <Text style={styles.tx}>Nombre:</Text>
+        <TextInput style={styles.Tex} placeholder='Nombre' value={nombre} onChangeText={setName} />
+        <Text style={styles.tx}>Email:</Text>
+        <TextInput style={styles.Tex} placeholder='Email' value={ema} onChangeText={setEmail}/>
+        <Text style={styles.tx}>Password:</Text>
+        <TextInput style={styles.Tex} placeholder='Password' value={pas} onChangeText={setPassword} secureTextEntry />
+        <Button title='Guardar' style={styles.boton} onPress={ handleSave } color="#000000"/>
+
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#c3c3c3',
     alignItems: 'center',
     justifyContent: 'center',
+    
+    padding: 20,  
+    borderRadius: 10,  
+  },
+  Tex: {
+    width: '100%',
+    height: 40,
+    backgroundColor: '#B5B2B2',
+    borderRadius: 5,
+    color: '#F4F4F4',
+    marginBottom: 20,
+    paddingLeft: 10,  
+  },
+  tx: {
+    color: 'black',
+    textAlign: 'left',  
+    alignSelf: 'flex-start',  
+    marginBottom: 5,
+  },
+  contb:{
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+    width: '80%',
+    shadowColor: '#000',
+    },
+
+  boton: {
+    backgroundColor: 'black',
+    padding: 15,
+    borderRadius: 5,
+    alignItems: 'center',
+    marginTop: 20,
+    width: '100%',
+  },
+  botont: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 16,
   },
 });
